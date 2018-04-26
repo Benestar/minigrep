d6e26eca627a4f413f9c90831537ce91ddb5824d
Benedikt Seidl <benedikt.seidl@gmail.com>
2018-04-26 17:29:27 +0200

    Implement minigrep

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+
+pub struct Config<'a> {
+    pub query: &'a str,
+    pub filename: &'a str
+}
+
+impl<'a> Config<'a> {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = &args[1];
+        let filename = &args[2];
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
